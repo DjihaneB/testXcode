08a6a907a65fae1fc814965cb6cbc6d7ea5a1104
DjihaneB <bouizemdjihane1312@gmail.com>
2023-08-31 00:22:50 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by user on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
